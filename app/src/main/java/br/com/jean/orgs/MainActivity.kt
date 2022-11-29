66c9b7002a0dbd3fc474e6b1815222f34e606dd5
Jean Ferreira <jeangabrielferreira1@gmail.com>
2022-11-29 17:18:40 -0300

    Adicionado RecyclerView

--- app/src/main/java/br/com/jean/orgs/MainActivity.kt
+++ app/src/main/java/br/com/jean/orgs/MainActivity.kt
@@ -7,8 +7,15 @@ class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_main)
+
+//        val tvNomeProduto: TextView = findViewById(R.id.tvNomeProduto)
+//        val tvDescricaoProduto: TextView = findViewById(R.id.tvDescricaoProduto)
+//        val tvPrecoProduto: TextView = findViewById(R.id.tvPrecoProduto)
+//
+//        tvNomeProduto.text = "Cesta de Frutas"
+//        tvDescricaoProduto.text = "Uva, abacaxi e banana"
+//        tvPrecoProduto.text = "R$ 20,00"
     }
 
 }
