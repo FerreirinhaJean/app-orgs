60b7731ddb1ad6e31817d71b551a5157ad369f05
Jean Ferreira <jeangabrielferreira1@gmail.com>
2022-11-29 17:33:51 -0300

    Iniciando implementação do Adapter

--- app/src/main/java/br/com/jean/orgs/MainActivity.kt
+++ app/src/main/java/br/com/jean/orgs/MainActivity.kt
@@ -1,13 +1,19 @@
-package br.com.jean.orgs
+package br.com.jean.orgs.activity
 
 import android.app.Activity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
+import br.com.jean.orgs.R
+import br.com.jean.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rvListaProdutos: RecyclerView = findViewById(R.id.rvListaProdutos)
+        rvListaProdutos.adapter = ListaProdutosAdapter()
 
 //        val tvNomeProduto: TextView = findViewById(R.id.tvNomeProduto)
 //        val tvDescricaoProduto: TextView = findViewById(R.id.tvDescricaoProduto)
