c56bad436fd6e5d140014bb5f444115614716ca9
Jean Gabriel Ferreira <jeangabrielferreira1@gmail.com>
2022-11-29 19:08:00 -0300

    Criação Activity para Formulário Produto

---
+++ app/src/main/java/br/com/jean/orgs/activity/FormularioProdutoActivity.kt
@@ -0,0 +1,14 @@
+package br.com.jean.orgs.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import br.com.jean.orgs.R
+
+class FormularioProdutoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_formulario_produto)
+    }
+
+}
