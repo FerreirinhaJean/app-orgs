9ce9acd0eaf0a6310b9ee77a19162ecc593788ed
Jean Gabriel Ferreira <jeangabrielferreira1@gmail.com>
2022-11-30 00:07:31 -0300

    Criado intent entre main e formulario

--- app/src/main/java/br/com/jean/orgs/activity/FormularioProdutoActivity.kt
+++ app/src/main/java/br/com/jean/orgs/activity/FormularioProdutoActivity.kt
@@ -2,12 +2,42 @@ package br.com.jean.orgs.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import br.com.jean.orgs.R
+import br.com.jean.orgs.model.Produto
+import java.math.BigDecimal
 
 class FormularioProdutoActivity : AppCompatActivity(R.layout.activity_formulario_produto) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val etNomeProduto = findViewById<EditText>(R.id.etNomeProduto)
+        val etDescricaoProduto = findViewById<EditText>(R.id.etDescricaoProduto)
+        val etPrecoProduto = findViewById<EditText>(R.id.etPrecoProduto)
+        val btSalvar = findViewById<Button>(R.id.btSalvar)
+
+        btSalvar.setOnClickListener {
+            val nome = etNomeProduto.text.toString()
+            val descricao = etDescricaoProduto.text.toString()
+            val preco = etPrecoProduto.text.toString()
+
+            val valor = if (preco.isBlank())
+                BigDecimal.ZERO
+            else
+                BigDecimal(preco)
+
+            val produto = Produto(
+                nome = nome,
+                descricao = descricao,
+                valor = valor
+            )
+
+
+        }
+
+
     }
 
 }
--- app/src/main/java/br/com/jean/orgs/activity/MainActivity.kt
+++ app/src/main/java/br/com/jean/orgs/activity/MainActivity.kt
@@ -1,5 +1,6 @@
 package br.com.jean.orgs.activity
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -7,6 +8,7 @@ import androidx.recyclerview.widget.RecyclerView
 import br.com.jean.orgs.R
 import br.com.jean.orgs.model.Produto
 import br.com.jean.orgs.ui.recyclerview.adapter.ListaProdutosAdapter
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import java.math.BigDecimal
 
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
@@ -31,8 +33,13 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
             this
         )
 
-
         rvListaProdutos.layoutManager = LinearLayoutManager(this)
+
+        val floatingActionButton = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        floatingActionButton.setOnClickListener{
+            val intent = Intent(this, FormularioProdutoActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
