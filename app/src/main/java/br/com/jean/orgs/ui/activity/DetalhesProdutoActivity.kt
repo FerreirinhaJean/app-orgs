3cb8bba34be195b30f3bb72e26755a97a8f74f35
Jean Ferreira <jeangabrielferreira1@gmail.com>
2022-12-06 08:11:17 -0300

    Adicionado listeners para opções do menu

--- app/src/main/java/br/com/jean/orgs/ui/activity/DetalhesProdutoActivity.kt
+++ app/src/main/java/br/com/jean/orgs/ui/activity/DetalhesProdutoActivity.kt
@@ -2,8 +2,10 @@ package br.com.jean.orgs.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuInflater
+import android.view.MenuItem
 import br.com.jean.orgs.R
 import br.com.jean.orgs.databinding.ActivityDetalhesProdutoBinding
 import br.com.jean.orgs.extensions.formataParaMoedaBrasileira
@@ -11,6 +13,8 @@ import br.com.jean.orgs.extensions.tentaCarregarImagem
 import br.com.jean.orgs.model.Produto
 
 class DetalhesProdutoActivity : AppCompatActivity() {
+
+    private val TAG = "DetalhesProdutoActivity"
 
     private val binding by lazy {
         ActivityDetalhesProdutoBinding.inflate(layoutInflater)
@@ -29,8 +33,22 @@ class DetalhesProdutoActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        MenuInflater(this).inflate(R.menu.menu_detalhes_produto, menu)
+        menuInflater.inflate(R.menu.menu_detalhes_produto, menu)
         return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_detalhes_produto_editar -> {
+                Log.i(TAG, "onOptionsItemSelected: editar")
+            }
+            R.id.menu_detalhes_produto_remover -> {
+                Log.i(TAG, "onOptionsItemSelected: remover")
+            }
+        }
+
+
+        return super.onOptionsItemSelected(item)
     }
 
 
